a8580f99e53b71dcefe57030b4d64c1850e670f2
younjinyoung <yhmun8151@gmail.com>
2018-04-03 15:07:44 +0900

    init project

---
+++ src/main/java/jinda/sytes/net/git_tutorial/GitTutorialApplication.java
@@ -0,0 +1,12 @@
+package jinda.sytes.net.git_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTutorialApplication.class, args);
+	}
+}
